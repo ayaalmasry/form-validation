122656a07338b77f00e097d76246dcb9c7ad5778
ayaalmasri <aya@aya.com>
2023-04-05 21:25:08 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {EmpAddEditComponent} from './emp-add-edit/emp-add-edit.component';
+
+import { FormBuilder,Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ constructor(private fb:FormBuilder ){}
   title = 'my-app';
+  userForm=this.fb.group({
+   firstName:['',Validators.required],
+   lastName:['',Validators.required],
+   
+   address:this.fb.group({
+     street:['',Validators.required],
+     postcode:['',Validators.required]
+   })
+   
+    
+  
+  });
+  addUser(){
+  console.log(this.userForm.value);
+  }
+ 
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                  //api
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpAddEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    CardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
